d3c3863fd33a21c84aca2a8def93cfb3999bbd42
23ds3000140 <167706710+23ds3000140@users.noreply.github.com>
2024-04-21 23:31:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,16 @@ def get_largest():
     num2 = cnt_children = st.number_input("NUM2")
     num3 = cnt_children = st.number_input("NUM3")
 
+    output = 0
     if num1 > num2 and num1 > num3:
-        return num1
+        return output = num1
     elif num2 > num1 and num2 > num3:
-        return num2
-    return num3
+        return output = num2
+    else
+        output = num3
+
+    st.subheader('Largest number is:')
+    st.write(output)
     
 
-output = get_largest()
-
-st.subheader('Largest number is:')
-st.write(output)
+st.form_submit_button(label="Submit", help=None, on_click=get_largest(), args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
