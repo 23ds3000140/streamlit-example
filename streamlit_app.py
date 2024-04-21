7ab4745a586bba5df2b1902bcb15bce973b95e54
23ds3000140 <167706710+23ds3000140@users.noreply.github.com>
2024-04-21 23:31:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,27 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.write("""
+# Largest of 3 numbers
+""")
+#Get Input
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.header('User Input Parameters')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def get_largest():
+    num1 = cnt_children = st.number_input("NUM1")
+    num2 = cnt_children = st.number_input("NUM2")
+    num3 = cnt_children = st.number_input("NUM3")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    output = 0
+    if num1 > num2 and num1 > num3:
+        return output = num1
+    elif num2 > num1 and num2 > num3:
+        return output = num2
+    else
+        output = num3
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    st.subheader('Largest number is:')
+    st.write(output)
+    
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.form_submit_button(label="Submit", help=None, on_click=get_largest(), args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
